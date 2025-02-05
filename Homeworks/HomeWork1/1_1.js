929aa2c9ca9220162a0b5a40c4d492fd8216e327
DanyOk32 <danyaverchenko@gmail.com>
2025-02-05 22:01:29 +0200

    create homework 1_1

---
+++ Homeworks/HomeWork1/1_1.js
@@ -0,0 +1,21 @@
+// Створити змінні. Присвоїти кожному з них значення: ---- +
+// 'hello','owu','com', 'ua', 1, 10, -999, 123, 3.14, 2.7, 16, true, false.
+// Вивести кожну змінну за допомогою: console.log
+let x = 'hello'
+let q = 'owu'
+let w = 'com'
+let r = 'ua'
+let one = 1
+let ten = 10
+let z = -999
+let triple = 123
+let PI = 3.14
+let ts = 2.7
+let o = 16
+let bt = 5>3
+let bf = !0
+
+console.log(x, q,w,r,one,ten)
+console.log(z,triple,PI)
+console.log(ts,o,bt)
+console.log(bf)
