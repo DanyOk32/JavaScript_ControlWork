c1db2adee080e3ade5ea1fdd95b59d3cdd9f14c9
DanyOk32 <danyaverchenko@gmail.com>
2025-02-05 22:22:18 +0200

    create 1_2

---
+++ Homeworks/HomeWork1/1_3/1_3.js
@@ -0,0 +1,12 @@
+// За допомогою оператора typeof
+// визначити типи наступних змінних та вивести їх в консоль.
+// let a = 100; let b = '100'; let c = true;
+
+let a = 100
+let b = '100'
+let c = true
+
+console.log(typeof(a),typeof(b),typeof(c))
+console.log(typeof(a))
+console.log(typeof(b))
+console.log(typeof(c))
