411178961655db63a669a40f568073abd6d40002
DanyOk32 <danyaverchenko@gmail.com>
2025-02-09 21:28:43 +0200

    update 2_11

--- Homeworks/HomeWork2/2_11/2_11.js
+++ Homeworks/HomeWork2/2_11/2_11.js
@@ -1,16 +1,28 @@
-//є змінна х, яка може прийняти будь-яке значення
-//(стрінг, число, undefined, null  і тд включно). Напишіть код який,
-//буде присвоювати змінній х значення "default"
+// є змінна х, яка може прийняти будь-яке значення
+// (стрінг, число, undefined, null  і тд включно). Напишіть код який,
+// буде присвоювати змінній х значення "default"
 // якщо ви намагаєтесь присвоїти в неї falsy-значення
 // (хибноподібні, тобто ті, які приводиться до false, а це 0 null undefined і тд).
 
 'use strict';
 // debugger
-let x = prompt("Будь-яке значення")
-if(x === 0 || x === -0 || x === 'false' || x === 0n || x=== null || x === undefined){
-    x = 'default';
+// false
+// 0
+// -0
+// ''
+// ""
+// ``
+// null
+// undefined
+// NaN
+
+let x = prompt("Введіть будь-що")
+if (x==='false' || x==='0' || x==='-0' || x=== "" || x=== '' || x===`` ||
+    x=== null || x=== 'null' || x=== undefined || x=== 'undefined'|| x === NaN ||
+    x==="NaN"|| x ===0 || x=== -0){
+    x='default'
     console.log(x)
-}else{
-    x='complete';
+} else{
+    x='Not default'
     console.log(x)
 }
