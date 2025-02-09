f6e3820feb7cafc2fb5c07ef53b47e8e3e1df61e
DanyOk32 <danyaverchenko@gmail.com>
2025-02-09 22:47:51 +0200

    add 3_3

---
+++ Homeworks/HomeWork3/3_3/3_3.js
@@ -0,0 +1,9 @@
+//За допомогою циклу while вивести в документ 20 блоків h1 c довільним текстом всередині.
+'use strict';
+// debugger
+
+let x = 1;
+while (x <= 20) {
+    document.write(`<div><h1>Okten great work</h1> ${x}</div>`)
+    x++;
+}
