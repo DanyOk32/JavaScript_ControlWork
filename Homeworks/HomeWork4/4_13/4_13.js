e3627a5cbaf4fe33fdfeda29f9394a7e4e4689f5
DanyOk32 <danyaverchenko@gmail.com>
2025-02-13 22:01:51 +0200

    add 4_13

---
+++ Homeworks/HomeWork4/4_13/4_13.js
@@ -0,0 +1,28 @@
+// - Написати функцію обміну валюти exchange(sumUAH,currencyValues,exchangeCurrency)
+// Приклад
+// exchange(10000,[{currency:'USD',value:25}
+// {currency:'EUR',value:42}],'USD')
+// => 400
+
+let x = [
+    {
+        currency: 'USD',
+        value: 41.79,
+    },
+    {
+        currency: 'EUR',
+        value: 43.40,
+    },
+    {
+        currency: 'JPY',
+        value: 0.27,
+    }
+]
+function exchange(sumUAH, array, exchangeCurrency){
+    for(let i = 0; i < array.length; i++){
+        if(array[i].currency === exchangeCurrency){
+            return sumUAH / array[i].value;
+        }
+    }
+}
+document.write(exchange(10000, x, 'JPY'))
