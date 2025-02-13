3cf3a98362514b8278a0f009459a6ae0891753d1
DanyOk32 <danyaverchenko@gmail.com>
2025-02-13 20:04:52 +0200

    add 4_2

---
+++ Homeworks/HomeWork4/4_2/4_2.js
@@ -0,0 +1,9 @@
+// - створити функцію яка обчислює та повертає площу кола з радіусом r
+'use strict';
+// debugger
+
+function squareCircle(r){
+    let Pi = 3.14;
+    return Pi * r * r;
+}
+document.write(squareCircle(10))
