6112a390a582a6611311d31d6772a78ea469f934
DanyOk32 <danyaverchenko@gmail.com>
2025-02-13 21:34:06 +0200

    add 4_11

---
+++ Homeworks/HomeWork4/4_11/4_11.js
@@ -0,0 +1,14 @@
+// - створити функцію sum(arr) яка приймає масив чисел
+// сумує значення елементів масиву та повертає його. Приклад sum([1,2,10]) //->13
+'use strict';
+// debugger
+
+let x=[123123123, 12, 919, 92131239, 9123123, -99999999, -29999, -124348415]
+function summ(arr){
+    let answer = 0;
+    for(let i = 0; i < arr.length; i++){
+        answer = answer + arr[i]
+    }
+document.write(answer)
+}
+summ(x)
